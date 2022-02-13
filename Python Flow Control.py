75b34506151cc475cbaf2fe861c27cca47b2647c
Pascalpedro <Attamapascalpedro@gmail.com>
2022-02-13 12:02:11 +0100

    Flow control Practices

---
+++ Python Flow Control.py
@@ -0,0 +1,88 @@
+# print("Hello, World!"); print("This is second line")     #To write two separate executable statements in a single line, you should use a semicolon ; to separate the commands
+#
+#
+# vowels = ['a', 'e', 'i',     #Expressions enclosed in ( ), [ ] or { } brackets don't need a backward slash for line continuation
+#           'o', 'u']
+#
+# print(vowels)
+
+
+# import sys
+#
+# if sys.version_info.major<3:
+#     print("version 2.x")
+# elif sys.version_info.major>3:
+#     print("future")
+# else:
+#     print("version 3.x")
+
+
+# for i in ['Hello', 'Come']:
+#     print(i)
+
+
+# x, y = 2, 8
+#
+# st = "x is less than y" if (x < y) else "x is greater than y"
+#
+# print(st)
+# if x < y:
+#     print("x is less than y")
+# elif x == y:
+#     print("x is equal to y")
+# else:
+#     print("x is greater than y")
+
+
+# prod = 1
+# i = 1
+# while i<10:
+#     prod = prod *1
+#     i += 1
+# print(prod)
+
+
+# for n in range(2, 10):
+#     if n % 2 == 0:
+#         print("found an even number", n)
+#         continue
+#     if n > 5:
+#         print("GT S!")
+#         break
+
+
+# python = iter("python")
+# print(python)
+
+
+# s = []
+#
+# for x in range(10):
+#     if x**2 >3:
+#         s.append(2 * x)
+# print(s)
+
+# s = [2 * x for x in range(10)if x **2 >3]
+# print(s)
+
+
+
+# x = range(10)
+# for n in x:
+#      print(n)
+
+
+# pairs = [(x, y) for x in range(4) for y in range(4)]
+# sing = set([(x, y) for x in range(4) for y in range(4)])
+# print(pairs)
+# print(sing)
+
+
+# ls = {s: len(s) for s in ['python', 'Javascript', 'Golang']}
+# print(ls)
+
+# pro = ['Python', 'Javascript', 'Golang']
+# ls = dict()
+#
+# for s in pro:
+#     if
