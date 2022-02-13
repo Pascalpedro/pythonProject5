d71f44299154f3009289e39ed7db58087a866f75
Pascalpedro <Attamapascalpedro@gmail.com>
2022-02-13 12:08:40 +0100

    Flow control Practices

---
+++ Python Functions.py
@@ -0,0 +1,19 @@
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+#
+# my_function("Emil", "Refsnes")
+#
+#
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+#
+# my_function("Emil")
+
+
+name = 'Pascal.pedro'
+
+print('I love ' + name)
+print('I love {}'.format(name))
+print(f'I love {name}')
+print(''.join(('I love ', name)))
+print('I love %s' %name)
